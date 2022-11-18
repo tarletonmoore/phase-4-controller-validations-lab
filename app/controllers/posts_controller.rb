50589c3340fc326923285faacef46c60e49fd643
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-18 13:42:22 -0500

    commit tree 42b9007f60d9c6d4cd3d9894384759329c69454d

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -7,17 +8,25 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
 
+  def find_post
+    Post.find(params[:id])
+  end
+
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
